b6fab1697ea5704b21cb8f8cab8aac98b7e50afb
Rishav <academicdahal21@gmail.com>
2024-08-25 12:52:42 +0545

    Updated Room form
    added theme to create and update room from

--- base/views.py
+++ base/views.py
@@ -140,30 +140,47 @@ def userProfile(request,pk):
 @login_required(login_url='login') # login_url='login' means that if the user is not logged in, they will be redirected to the login page
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name) # get_or_create() will get the object if it exists or create it if it does not exist
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user
+        #     room.save()
+        return redirect('home')
+    
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room) # instance=room means that the form will be pre-filled with the data from the room object
-    
+    topics = Topic.objects.all()
     if request.user != room.host : # this will check if the user is the host of the room
         return HttpResponse('You are not allowed here!')
     
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room) # instance=room means that the form will update the room object
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        
+        # form = RoomForm(request.POST, instance=room) # instance=room means that the form will update the room object
+        # if form.is_valid():
+        #     form.save()
+        return redirect('home')
+    context = {'form': form , 'topics': topics , 'room': room}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
