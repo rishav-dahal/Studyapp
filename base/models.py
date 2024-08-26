94156eae9719e78d8cff4076d20468ad66d6a007
Rishav <academicdahal21@gmail.com>
2024-08-27 00:18:38 +0545

    updated avatar urls

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
@@ -7,7 +8,12 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null=True, blank=True, upload_to='avatars/', default='avatars.svg')
+    def generate_filename(instance, filename):
+        extension = filename.split('.')[-1]
+        unique_filename = f'{instance.email}_{instance.id}.{extension}'
+        return f'avatars/{unique_filename}'
+
+    avatar = models.ImageField(null=True, blank=True, upload_to=generate_filename, default='avatars.svg')
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
